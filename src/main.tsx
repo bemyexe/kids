b90019ee0bdad9ee0777c5ed6aab62943f83bbce
maxim <trifonov.max@mail.ru>
2025-06-05 10:15:59 +0300

    base

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { App } from './app';
+
+import './global-styles/reset.scss';
+import './global-styles/colors.scss';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
