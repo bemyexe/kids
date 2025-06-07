0f05ac4e38bcafcc24e8ab0e9b455bb3a6cf9caa
maxim <trifonov.max@mail.ru>
2025-06-07 13:34:03 +0300

    ref: upload

--- src/shared/ui/upload/index.tsx
+++ src/shared/ui/upload/index.tsx
@@ -1,8 +1,8 @@
-import { type ComponentProps, useId } from 'react';
+import {type ComponentProps, useId} from 'react';
 import clsx from 'clsx';
 
 import Download from '../../../assets/download.svg?react';
-import { Typography } from '../typography';
+import {Typography} from '../typography';
 
 import './style.scss';
 
@@ -11,17 +11,19 @@ interface Props extends ComponentProps<'input'> {
   className?: string;
 }
 
-export const Upload = ({ label, className }: Props) => {
+export const Upload = ({label, className}: Props) => {
   const id = useId();
 
   return (
-    <div className={clsx('upload', className)}>
-      <label className="upload__label" htmlFor={id}>
-        <div className="upload__label-icon-wrapper">
-          <Download stroke="#293244" />
-        </div>
-      </label>
-      <input id={id} type="file" hidden />
+    <div className={clsx('upload-container', className)}>
+      <div className="upload-container__wrapper">
+        <label htmlFor={id}>
+          <div className="upload-container__wrapper-icon">
+            <Download stroke="#293244" />
+          </div>
+          <input id={id} type="file" hidden />
+        </label>
+      </div>
       <Typography>{label}</Typography>
     </div>
   );
