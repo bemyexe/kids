ad81d20bad0b8d04d8e3e8fffa7e23b80c2e2645
maxim <trifonov.max@mail.ru>
2025-06-07 15:30:17 +0300

    ref: upload , upload-page

--- src/app/pages/upload-page/index.tsx
+++ src/app/pages/upload-page/index.tsx
@@ -1,30 +1,80 @@
+import {useForm} from 'react-hook-form';
+
 import ExclamationMarkIcon from '../../../assets/exclamation-mark.svg?react';
 import RightIcon from '../../../assets/right-arrow.svg?react';
-import { Button } from '../../../shared/ui/button';
-import { Chip } from '../../../shared/ui/chip';
-import { Typography } from '../../../shared/ui/typography';
-import { Upload } from '../../../shared/ui/upload';
+import {Button} from '../../../shared/ui/button';
+import {Chip} from '../../../shared/ui/chip';
+import {Typography} from '../../../shared/ui/typography';
+import {Upload} from '../../../shared/ui/upload';
 
 import './style.scss';
 
+const CHIP_LABEL =
+  'Допустимые форматы файлов: jpg, jpeg, png, pdf. Размер не более 5 Мб';
+
+type FormData = {
+  houseTreePerson: FileList;
+  nonexistentAnimal: FileList;
+  selfPortrait: FileList;
+};
+
 export const UploadPage = () => {
+  const {
+    register,
+    watch,
+    formState: {isValid},
+  } = useForm<FormData>({
+    mode: 'onChange',
+  });
+
+  const houseTreePerson = watch('houseTreePerson');
+  const nonexistentAnimal = watch('nonexistentAnimal');
+  const selfPortrait = watch('selfPortrait');
+
   return (
-    <div className="upload-page">
-      <Typography tag="h3" size="20">
-        Загрузите фотографии рисунков
-      </Typography>
+    <main className="upload-page">
+      <div className="upload-page__top">
+        <Typography tag="h3" size="20">
+          Загрузите фотографии рисунков
+        </Typography>
+        <Chip
+          variant="warning"
+          label={CHIP_LABEL}
+          leftIcon={<ExclamationMarkIcon />}
+        />
+      </div>
 
-      <Typography size="14" weight="bold">
-        Шаг 1/3
-      </Typography>
-      <Button rightIcon={<RightIcon stroke="red" />}>Далее</Button>
-      <Chip
-        variant="warning"
-        label="Нужно загрузить хотя бы 3 фотографии"
-        leftIcon={<ExclamationMarkIcon />}
-      />
+      <section className="upload-page__content">
+        <Upload
+          label={'Дом, дерево, человек'}
+          {...register('houseTreePerson', {required: true})}
+          file={houseTreePerson?.[0]}
+        />
+        <Upload
+          label={'Несуществующее животное'}
+          {...register('nonexistentAnimal', {required: true})}
+          file={nonexistentAnimal?.[0]}
+        />
+        <Upload
+          label={'Автопортрет'}
+          {...register('selfPortrait', {required: true})}
+          file={selfPortrait?.[0]}
+        />
+      </section>
 
-      <Upload label={'Автопортрет'} />
-    </div>
+      <div className="upload-page__bottom">
+        <Typography
+          className="upload-page__bottom-text"
+          size="14"
+          weight="bold">
+          Шаг 1/3
+        </Typography>
+        <Button
+          rightIcon={<RightIcon stroke={isValid ? 'white' : '#44537180'} />}
+          disabled={!isValid}>
+          Далее
+        </Button>
+      </div>
+    </main>
   );
 };
--- src/shared/ui/upload/index.tsx
+++ src/shared/ui/upload/index.tsx
@@ -1,27 +1,69 @@
-import {type ComponentProps, useId} from 'react';
+import {type ComponentProps, useEffect, useId, useState} from 'react';
 import clsx from 'clsx';
 
 import Download from '../../../assets/download.svg?react';
+import Refresh from '../../../assets/refresh.svg?react';
 import {Typography} from '../typography';
 
 import './style.scss';
 
+const MAX_FILE_SIZE = 5 * 1024 * 1024;
+
 interface Props extends ComponentProps<'input'> {
   label: string;
+  file?: File;
   className?: string;
 }
 
-export const Upload = ({label, className}: Props) => {
+export const Upload = ({label, file, className, ...props}: Props) => {
   const id = useId();
+  const [preview, setPreview] = useState<string | null>(null);
+
+  useEffect(() => {
+    setPreview(null);
+
+    if (file) {
+      if (file.size > MAX_FILE_SIZE) {
+        alert('Файл слишком большой. Максимальный размер: 5 МБ');
+        return;
+      }
+      const reader = new FileReader();
+      reader.onload = (e) => setPreview(e.target?.result as string);
+      reader.readAsDataURL(file);
+
+      return () => {
+        reader.abort();
+      };
+    }
+  }, [file]);
 
   return (
     <div className={clsx('upload-container', className)}>
-      <div className="upload-container__wrapper">
+      <div className="input-container">
+        {preview && (
+          <img className="input-container__image" src={preview} alt="preview" />
+        )}
         <label htmlFor={id}>
-          <div className="upload-container__wrapper-icon">
-            <Download stroke="#293244" />
+          <div className="input-container__wrapper">
+            {file ? (
+              <Refresh
+                className="input-container__wrapper-icon--refresh"
+                stroke="#293244"
+              />
+            ) : (
+              <Download
+                className="input-container__wrapper-icon--download"
+                stroke="#293244"
+              />
+            )}
           </div>
-          <input id={id} type="file" hidden />
+          <input
+            id={id}
+            type="file"
+            hidden
+            accept="image/jpeg,image/png,image/jpg,application/pdf"
+            {...props}
+          />
         </label>
       </div>
       <Typography>{label}</Typography>
